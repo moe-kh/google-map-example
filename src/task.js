7c0f523ce554533902ea8bd14e6faab8f4a3c033
Mahmoud Khaled <59619325+moe-kh@users.noreply.github.com>
2020-12-22 10:30:30 -0700

    first commit

---
+++ src/task.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const task = ( props ) => {
+
+    return (
+       
+   <li>
+     
+
+      <p> {props.value}</p>
+     
+             
+    </li>
+      
+       
+    )
+};
+
+export default task;
